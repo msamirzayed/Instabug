4a7059411d85fddd490c576a108880f3f9f23e9b
Mahmoud Samir <mahmoudsamir1924@gmail.com>
2022-05-30 00:05:38 +0200

    form design

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let users = [
+const users = [
   { email: "mohamed@instabug.com", password: "A12345678" },
   { email: "mohamed1@instabug.com", password: "A12345678" },
   { email: "mohamed2@instabug.com", password: "A12345678" },
@@ -8,3 +8,37 @@ let users = [
   { email: "mohamed6@instabug.com", password: "A12345678" },
   { email: "mohamed7@instabug.com", password: "A12345678" },
 ];
+let userMail = "";
+let userPass = "";
+let auth = false;
+
+if (!localStorage.getItem("userID")) {
+  auth = false;
+  login();
+} else {
+  auth = true;
+  welcome(localStorage.getItem("userID"));
+}
+
+function login() {
+  check();
+}
+
+function logout() {
+  localStorage.removeItem("userID", userMail);
+}
+
+function check() {
+  users.filter((e) => {
+    if (e.email == userMail && e.password == userPass) {
+      localStorage.setItem("userID", userMail);
+      auth = true;
+    } else {
+      console.log("no matched user in the db");
+    }
+  });
+}
+
+function welcome(user) {
+  console.log("welcome " + user);
+}
