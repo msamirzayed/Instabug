a86a29e131d9639adaf88ac660817ebbd9d76dcd
Mahmoud Samir <mahmoudsamir1924@gmail.com>
2022-05-30 20:57:56 +0200

    without redirection

--- src/app.js
+++ src/app.js
@@ -3,31 +3,15 @@ let auth = false;
 
 document.addEventListener("DOMContentLoaded", function () {
   currentPath = window.location.pathname;
-  currentHost = window.location.host;
-  currentLoc = currentHost + currentPath;
-  console.log(window.location);
-  console.log(currentLoc);
-  window.location.replace("Instabug" + "/login.html");
-  // console.log("/login.html");
-  // if (!localStorage.getItem("userID")) {
-  //   auth = false;
-  //   login();
-  // } else if (localStorage.getItem("userID")) {
-  //   auth = true;
-  //   welcome(localStorage.getItem("userID"));
-  // }
+  if (!localStorage.getItem("userID")) {
+    auth = false;
+    login();
+  } else if (localStorage.getItem("userID")) {
+    auth = true;
+    welcome(localStorage.getItem("userID"));
+  }
 });
 
-// function login() {
-//   if (currentPath != "/login.html" && currentPath != "/error.html") {
-//     window.location.replace("/login.html");
-//     console.log("/login.html");
-//   }
-// }
+function login() {}
 
-// function welcome(user) {
-//   if (auth == true && currentPath != "/") {
-//     window.location.replace("/");
-//   }
-//   console.log("welcome " + user);
-// }
+function welcome(user) {}
